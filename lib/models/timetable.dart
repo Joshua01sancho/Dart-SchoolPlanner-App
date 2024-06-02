dc2be02bf0bd781ad3ae5fb866071a4f379300cb
Joshua01sancho <126288844+Joshua01sancho@users.noreply.github.com>
2024-06-02 18:41:52 +0200

    Update and rename blank.txt to timetable.dart
    Timetable class and it will be Extended in the main.dart

---
+++ lib/models/timetable.dart
@@ -0,0 +1,16 @@
+class Timetable {
+  String subject;
+  String location;
+  String startTime;
+  String endTime;
+  String dayOfWeek;
+
+  Timetable({
+    required this.subject,
+    required this.location,
+    required this.startTime,
+    required this.endTime,
+    required this.dayOfWeek,
+  });
+}
+
